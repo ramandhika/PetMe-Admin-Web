ef1eded97a6fd84d72d3c595fc80f8b9e01b13e6
Ramandhika Ilham Pratama <ramandhika.ilham@gmail.com>
2023-12-08 02:09:41 +0700

    Update sidebar height in layout

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -1,5 +1,5 @@
 <body class="bg-gray-100 -z-20">
-    <div class="w-full h-12 bg-purple-custom absolute -z-10 py-36"></div>
+    <div class="w-full h-12 bg-purple-custom absolute -z-10 py-40"></div>
     <div class="flex z-50">
         <div class="w-1/5 bg-white p-8 pt-14 pb-14 ml-20 mt-20 rounded-t-3xl text-xl font-bold text-center shadow-xl">
             <!-- Logo -->
