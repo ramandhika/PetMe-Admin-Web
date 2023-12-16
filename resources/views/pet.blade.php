015f829a2b18ccd0fc42bf5b3d31bfb2b8ccb65e
Ramandhika Ilham Pratama <ramandhika.ilham@gmail.com>
2023-12-17 03:17:57 +0700

    Add delete confirmation modal

--- resources/views/pet.blade.php
+++ resources/views/pet.blade.php
@@ -31,6 +31,7 @@
             placeholder="Search...">
     </div>
     <div class="flex w-full bg-white mt-5 px-10 h-95 rounded-3xl">
+
         <div class="container mx-auto">
             <div class="flex justify-end mb-5 pt-5">
                 <a href="{{ route('pet.index') }}" class="px-4 py-2 mx-2 bg-pastel-custom text-purple-custom rounded-xl hover:bg-purple-custom hover:text-pastel-custom font-semibold text-xl">All</a>
@@ -89,10 +90,10 @@
                                     <a href="{{ route('pet.index', $post->id) }}" class="border border-blue-500 text-blue-500 px-3.5 py-0.5 rounded-md ml-1 hover:bg-blue-500 hover:text-white">
                                         <i class="fas fa-eye"></i>
                                     </a>
-                                    <form method="POST" action="{{ route('pet.destroy', $post->id) }}">
+                                    <form id="deleteForm" method="POST" action="{{ route('pet.destroy', $post->id) }}">
                                         @csrf
                                         @method('DELETE')
-                                        <button type="submit" class="border border-red-500 text-red-500 px-3.5 py-0.5 rounded-md ml-1 hover:bg-red-500 hover:text-white">
+                                        <button type="button" onclick="showModal({{ $post->id }})" class="border border-red-500 text-red-500 px-3.5 py-0.5 rounded-md ml-1 hover:bg-red-500 hover:text-white">
                                             <i class="fas fa-trash"></i>
                                         </button>
                                     </form>
@@ -117,7 +118,43 @@
     </div>
 </div>
 
+<div id="popup-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-full max-h-full bg-opacity-50 bg-gray-500 backdrop-filter backdrop-blur-sm">
+    <div class="relative p-4 w-full max-w-md max-h-full">
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <button type="button" onclick="hideModal()" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
+                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                </svg>
+                <span class="sr-only">Close modal</span>
+            </button>
+            <div class="p-4 md:p-5 text-center">
+                <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                </svg>
+                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete ?</b></h3>
+                <button data-modal-hide="popup-modal" type="button" onclick="document.getElementById('deleteForm').submit();" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center me-2">
+                    Yes, I'm sure
+                </button>
+                <button data-modal-hide="popup-modal" type="button" onclick="hideModal()" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No, cancel</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 <script>
+    function showModal(id) {
+        const modal = document.getElementById('popup-modal');
+        modal.classList.remove('hidden');
+
+        const deleteForm = document.getElementById('deleteForm');
+        deleteForm.action = "{{ route('pet.destroy', '') }}" + id;
+    }
+
+    function hideModal() {
+        const modal = document.getElementById('popup-modal');
+        modal.classList.add('hidden');
+    }
+
     // Live Search
     const liveSearchInput = document.getElementById('liveSearchInput');
     const tableRows = document.querySelectorAll('.table-auto tbody tr');
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -80,16 +80,14 @@
                         </td>
                         <td class="py-1.5 px-6">
                             <div class="flex item-center justify-center">
-                                <form onsubmit="return confirm('Apakah Anda Yakin ?');"
-                                    action="{{ route('user.destroy', $user->id) }}" method="POST">
-                                    <a href="{{ route('user.edit', $user->id) }}"
-                                        class="border border-blue-500 text-blue-500 px-3.5 py-0.5 rounded-md hover:bg-blue-500 hover:text-white"><i
-                                            class="fas fa-edit"></i>Edit</a>
+                                <a href="{{ route('user.edit', $user->id) }}" class="border border-blue-500 text-blue-500 px-3.5 py-0.5 rounded-md hover:bg-blue-500 hover:text-white"><i
+                                class="fas fa-edit"></i> Edit</a>
+                                <form id="deleteForm" method="POST" action="{{ route('user.destroy', $user->id) }}">
                                     @csrf
                                     @method('DELETE')
-                                    <button
-                                        class="border border-red-500 text-red-500 px-3.5 py-0.5 rounded-md ml-2 hover:bg-red-500 hover:text-white"><i
-                                            class="fas fa-trash"></i> Hapus</button>
+                                    <button type="button" onclick="showModal({{ $user->id }})" class="border border-red-500 text-red-500 px-3.5 py-0.5 rounded-md ml-1 hover:bg-red-500 hover:text-white">
+                                        <i class="fas fa-trash"></i> Delete
+                                    </button>
                                 </form>
                             </div>
                         </td>
@@ -108,7 +106,44 @@
     </div>
 </div>
 
+<div id="popup-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full md:inset-0 h-full max-h-full bg-opacity-50 bg-gray-500 backdrop-filter backdrop-blur-sm">
+    <div class="relative p-4 w-full max-w-md max-h-full">
+        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <button type="button" onclick="hideModal()" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
+                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                </svg>
+                <span class="sr-only">Close modal</span>
+            </button>
+            <div class="p-4 md:p-5 text-center">
+                <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                </svg>
+                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete ?</b></h3>
+                <button data-modal-hide="popup-modal" type="button" onclick="document.getElementById('deleteForm').submit();" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center me-2">
+                    Yes, I'm sure
+                </button>
+                <button data-modal-hide="popup-modal" type="button" onclick="hideModal()" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No, cancel</button>
+            </div>
+        </div>
+    </div>
+</div>
+
 <script>
+    // Modal
+    function showModal(id) {
+        const modal = document.getElementById('popup-modal');
+        modal.classList.remove('hidden');
+
+        const deleteForm = document.getElementById('deleteForm');
+        deleteForm.action = "{{ route('user.destroy', '') }}" + '/' + id;
+    }
+
+    function hideModal() {
+        const modal = document.getElementById('popup-modal');
+        modal.classList.add('hidden');
+    }
+
     // Live Search
     const liveSearchInput = document.getElementById("liveSearchInput");
     const tables = document.querySelectorAll("table");
