fb207846df538eff42e7ed270b3832cad3566a4c
Ramandhika Ilham Pratama <ramandhika.ilham@gmail.com>
2023-12-18 22:23:32 +0700

    Update favicon link in head.blade.php

--- resources/views/layout/head.blade.php
+++ resources/views/layout/head.blade.php
@@ -1,7 +1,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-<link rel="icon" href="{{ asset('assets/img/favicon.ico') }}">
+<link rel="icon" href="https://storage.googleapis.com/petmebucket/assets/favicon.ico">
 {{-- <link rel="icon" href="assets/img/favicon.ico"> --}}
 <title>PetMe Admin Dashboard</title>
 {{-- @vite('./resources/css/app.css') --}}
